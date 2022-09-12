c6f34bf2e1013285d550063b86838e2fc639f567
Shelumiel <sssharon7@gmail.com>
2022-09-12 21:10:45 +0800

    Add user component

---
+++ pages/users.js
@@ -0,0 +1,29 @@
+import User from "../components/user";
+
+function UserList({ users }) {
+  return (
+    <div>
+      <h1>List of users</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <User user={user}></User>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default UserList;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
