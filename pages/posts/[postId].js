cc670723c409f9c3c0ca376c60ea82c5d3347d6f
Shelumiel <sssharon7@gmail.com>
2022-09-13 11:49:01 +0800

    Add getStaticPaths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -12,6 +12,23 @@ function Post({ post }) {
 
 export default Post;
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { postId: "1" },
+      },
+      {
+        params: { postId: "2" },
+      },
+      {
+        params: { postId: "3" },
+      },
+    ],
+    fallback: false,
+  };
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   const response = await fetch(
